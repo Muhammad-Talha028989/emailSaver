0904f27d048bd131867bee4ac2d8b5e2140c95c9
Muhammad-Talha028989 <104294684+Muhammad-Talha028989@users.noreply.github.com>
2022-11-10 15:05:13 +0500

    added the get personal data form in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './assets/components/Home.components/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import GetPersonalForm from './assets/components/GetPersonalInfo/GetPersonal.form'
+import PersonalInfoForm from './assets/components/EmailProtection/Personal.info.form'
 
 function App (): JSX.Element
 {
   const [ count, setCount ] = useState( 0 )
 
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={ <Home /> }>
+            <Route path='/' element={ <PersonalInfoForm /> } />
+            <Route path='/get-personal-data' element={ <GetPersonalForm /> } />
+          </Route>
+        </Routes>
+
+      </div>
+    </Router>
+
   )
 }
 
