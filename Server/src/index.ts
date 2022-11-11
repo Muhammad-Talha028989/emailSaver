54de24adc86f4d16b4738013b40d6b98c4b38b11
Muhammad-Talha028989 <104294684+Muhammad-Talha028989@users.noreply.github.com>
2022-11-11 17:20:22 +0500

    sucessful added the server-side-code into project

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,1 +1,10 @@
-console.info( "hello" )
+import express,{ Express,Request,Response } from "express";
+
+const app: Express = express()
+const port:number = 3000
+app.get( "", ( req: Request, res: Response ) =>
+{
+    res.send("Hello")
+} )
+
+app.listen( port, () => console.info( "Running at port 3000" ) )
