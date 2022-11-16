94412c77453e8283959bc52befc7e6583ccbc393
Muhammad-Talha028989 <104294684+Muhammad-Talha028989@users.noreply.github.com>
2022-11-16 14:37:57 +0500

    modified the home&personal

--- Server/src/assets/routes/Home.route.ts
+++ Server/src/assets/routes/Home.route.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import Cryptr from 'cryptr'
 import express,{ Router,Request,Response } from 'express';
 import { PersonalModel } from './../database/models/INF.models.schema';
-const cryptr = new Cryptr("Hello");
+const cryptr = new Cryptr(process.env.SIGNATUREKEY);
 const HomeRoute: Router = express.Router()
+
+
 
 HomeRoute.get( "/", ( req: Request, res: Response ): void =>
 {
@@ -16,17 +20,29 @@ HomeRoute.post( "/", ( req: Request, res: Response ): void =>
     let responseData = req?.body?.RequestData
     console.info( [ "request data", responseData ] );
     const password:string = cryptr.encrypt(responseData.password);
-    const secret:string = cryptr.encrypt(responseData.secretkey);
-    let newPersonalDoc = new PersonalModel( {
-        email: responseData?.email,
-        secret: secret,
-        password: password,
-        description: responseData?.description
-    } ).save()
-    PersonalModel.findOne( { email: responseData?.email } ).then( ( response ) =>
+    const secret: string = cryptr.encrypt( responseData.secretkey );
+    /* Here is logic for database accessed */
+    PersonalModel.find( { email: responseData?.email} ).then( ( response ) =>
     {
-        const secretKey:string = cryptr.decrypt(response.secret)
+        if ( response === null )
+        {
+            let newPersonalDoc = new PersonalModel( {
+                email: responseData?.email,
+                secret: secret,
+                password: password,
+                description: responseData?.description
+            } ).save()
+            console.info("Data added to database successful")
+            return 
+        }
+        console.info( "Here database information" );
     })
+
+    // PersonalModel.findOne( { email: responseData?.email } ).then( ( response ) =>
+    // {
+    //     const secretKey: string = cryptr.decrypt( response.secret )
+    //     console.info(secretKey)
+    // })
     res.send( { message: "Successful!", code: 200, isSuccessful: true } ).status( 200 )
 })
 
--- src/assets/components/EmailProtection/Personal.info.form.tsx
+++ src/assets/components/EmailProtection/Personal.info.form.tsx
@@ -23,7 +23,7 @@ const PersonalInfoForm: () => JSX.Element = (): JSX.Element =>
 
                             <div className="w-full">
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input onChange={ ( e ) => handleChangle( e, setFormState, FormState ) } type={ "text" } name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter the Password" required={ true } />
+                                <input onChange={ ( e ) => handleChangle( e, setFormState, FormState ) } type={ "password" } name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter the Password" required={ true } />
                             </div>
                             {/* Secret-Key input field */ }
 
