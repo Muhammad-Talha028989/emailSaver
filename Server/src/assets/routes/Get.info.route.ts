8dcb4b6d77a46c67a3a0845effda348f20b18bd7
Muhammad-Talha028989 <104294684+Muhammad-Talha028989@users.noreply.github.com>
2022-11-19 18:39:52 +0500

    successful validate and verfied the response data from server

--- Server/src/assets/routes/Get.info.route.ts
+++ Server/src/assets/routes/Get.info.route.ts
@@ -21,40 +21,107 @@ FetchRoute.post( "/", ( req: Request, res: Response ): void =>
 {
     let responseData = req?.body.RequestData;
     // console.info( [ "request data", responseData ] )
-    
+    let { email, secretkey } = req?.body?.RequestData;
     // let secret:string = cryptr.encrypt(responseData?.secretkey)
-    PersonalModel.findOne( { email: responseData?.email} ).then( resultResponse =>
+    if ( !email && secretkey)
     {
-        let secretDecrypt: string = cryptr.decrypt( resultResponse?.secret || "" );
-        let PasswordDecrptArray: Array<String> = [];
-        resultResponse?.password?.map( items =>
+        let isExist: Boolean = false; /* Mean found no secret matched */
+        PersonalModel.find( {}, {}, {} ).then( ( responseResult ) =>
         {
-            PasswordDecrptArray.push( cryptr.decrypt( items || "" ) );
-        }) 
-        if ( secretDecrypt === responseData?.secretkey )
-        {
-            const Errors: ErrorHandler = {
-                message: "Email Found Successful",
-                code: "OK" || "PASS" || "SUCCESSFUL",
-                status: 200
+            if ( responseResult )
+            {
+                responseResult?.map( ( emailDoc ) =>
+                {
+                    let secretDecrypt: string = cryptr.decrypt( emailDoc?.secret || "" )
+
+                    if ( secretDecrypt === responseData?.secretkey )
+                    {
+                        let PasswordDecrptArray: Array<String> = []
+                        emailDoc?.password?.map( items =>
+                        {
+                            PasswordDecrptArray.push( cryptr.decrypt( items || "" ) )
+                        } )
+
+                        const Errors: ErrorHandler = {
+                            message: "Email Found Successful",
+                            code: "OK" || "PASS" || "SUCCESSFUL",
+                            status: 200
+                        }
+                        const ResponseObject = {
+                            email: emailDoc?.email,
+                            password: PasswordDecrptArray,
+                            description: emailDoc?.description
+                        }
+                        isExist = true
+                        res.send( { ...Errors, ResponseObject } )
+                        return
+                    }
+                } )
             }
-            const ResponseObject = {
-                email: resultResponse?.email,
-                password: PasswordDecrptArray,
-                description: resultResponse?.description
-            }
-            res.send( {...Errors,ResponseObject});
-        }
-        else
-        {
+        } );
+        if (!isExist) {
             const Errors: ErrorHandler = {
                 message: "Email Not Found",
                 code: "FAIL" || "WROUNG" || "UNSUCCESSFUL",
-                status: 404 
-            };
-            res.send({...Errors,resData:{}})
+                status: 404
+            }
+            res.send( { ...Errors, resData: {} } );
         }
-    })
+    }
+    else if (email && secretkey) {
+        PersonalModel.findOne( { email: responseData?.email } ).then( resultResponse =>
+        {
+            if ( resultResponse )
+            {
+                let secretDecrypt: string = cryptr.decrypt( resultResponse?.secret || "" )
+                let PasswordDecrptArray: Array<String> = []
+                resultResponse?.password?.map( items =>
+                {
+                    PasswordDecrptArray.push( cryptr.decrypt( items || "" ) )
+                } )
+                if ( secretDecrypt === responseData?.secretkey )
+                {
+                    const Errors: ErrorHandler = {
+                        message: "Email Found Successful",
+                        code: "OK" || "PASS" || "SUCCESSFUL",
+                        status: 200
+                    }
+                    const ResponseObject = {
+                        email: resultResponse?.email,
+                        password: PasswordDecrptArray,
+                        description: resultResponse?.description
+                    }
+                    res.send( { ...Errors, ResponseObject } )
+                }
+                else
+                {
+                    const Errors: ErrorHandler = {
+                        message: "Email Not Found",
+                        code: "FAIL" || "WROUNG" || "UNSUCCESSFUL",
+                        status: 404
+                    }
+                    res.send( { ...Errors, resData: {} } )
+                } 
+            }
+            else
+            {
+                const Errors: ErrorHandler = {
+                    message: "Plese check the both inputs or must enter correct information",
+                    code: "FAIL" || "WROUNG" || "UNSUCCESSFUL",
+                    status: 404
+                };
+                res.send( { ...Errors, resData: {} } );
+            }
+
+        } )
+    } else {
+        const Errors: ErrorHandler = {
+            message: "Plese check the both inputs or must enter correct information",
+            code: "FAIL" || "WROUNG" || "UNSUCCESSFUL",
+            status: 404
+        };
+        res.send( { ...Errors, resData: {} } );
+    }
 })
 
 
