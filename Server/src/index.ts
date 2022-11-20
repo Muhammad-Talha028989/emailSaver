1119f05f3a8d43269b442500f5b7b14d7b7c7484
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-11-20 15:21:18 +0500

    modified the index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,14 +1,14 @@
 import * as dotenv from "dotenv"
 dotenv.config()
 
-import express, {Express, Request, Response } from "express"
+const express = require("express")
 import cors from "cors"
 import bodyParser, { urlencoded } from "body-parser";
 import HomeRoute from "./assets/routes/Home.route";
 import { createMongodbConnection } from './assets//database/index.database';
 import FetchRoute from './assets//routes/Get.info.route';
 
-const app:Express = express()
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
