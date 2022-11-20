a1ad5505611886aa668fe8a23ec49e48d4570de5
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-11-20 14:36:09 +0500

    update the dependec

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -15,7 +15,7 @@ app.use( express.json( {} ) );
 
 // for deployment
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("public"))
+    app.use(express.static("Client/dist"))
 }
 
 /* Mongoose code */
