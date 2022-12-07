6119e7bb59b06ce4460eb20eec0bda931dd621b0
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-12-07 14:49:32 +0500

    modified the Home.tsx

--- Server/Client/src/assets/components/Home.components/Home.tsx
+++ Server/Client/src/assets/components/Home.components/Home.tsx
@@ -15,19 +15,19 @@ const Home = () =>
 
     return (
         <>
-            <animated.div style={ styleForm } >
+            <animated.div style={ styleForm } className="flex justify-center w-[100%]">
                 <Link to={ "/" } className="inline-flex items-center justify-center px-5 py-3 ml-11 mt-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Go Back
+                    Back
                 </Link>
-                <Link to={ "/show-get-data" } className="inline-flex items-center justify-center px-5 py-3 ml-11 mt-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Show Result
+                <Link to={ "/show-get-data" } className="inline-flex  items-center justify-center px-5 py-3 ml-11 mt-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    Result
                 </Link>
             </animated.div>
             <section className="bg-white dark:bg-gray-900">
                 <animated.div style={ styleForm } className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div className="mr-auto place-self-center lg:col-span-7" >
-                        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
-                        <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+                        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Personal Web Application for EmailManagement</h1>
+                        <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Best Way to Remember the personal Information</p>
 
                         <Link to={ "/get-personal-data" } className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             Get Personal Information
